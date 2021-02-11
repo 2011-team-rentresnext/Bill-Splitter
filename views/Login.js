62a2f68e7272ce070c31f1c0b07c1b64023261ef
Matt Ellison <mattbryanellison@gmail.com>
2021-02-11 13:14:31 -0500

    [fixes] failed login bug

--- views/Login.js
+++ views/Login.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   Text,
   TextInput,
@@ -15,104 +15,118 @@ import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview'
 import {auth} from '../store'
 import {connect} from 'react-redux'
 
-class Login extends Component {
-  constructor() {
-    super()
-    this.state = {
-      email: '',
-      password: '',
+export function Login(props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginFailed, setLoginFailed] = useState(false)
+  let emailInput
+  let passwordInput
+
+  const handlePress = () => {
+    props.login(email, password)
+  }
+
+  useEffect(() => {
+    if (props.user.id) props.navigation.navigate('UserHome')
+    if (props.user.error) {
+      setLoginFailed(true)
+      emailInput.clear()
+      passwordInput.clear()
     }
-  }
+  }, [props.user])
 
-  handlePress = () => {
-    this.props.login(this.state.email, this.state.password)
-    this.props.navigation.navigate('UserHome')
-  }
+  useEffect(() => {
+    setLoginFailed(false)
+  }, [])
 
-  render() {
-    return (
-      <KeyboardAwareScrollView contentContainerStyle={{flex: 1}}>
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'white',
-              alignItems: 'center',
-              justifyContent: 'space-around',
-              height: '100%',
-            }}
-          >
-            <View>
-              <Text
-                style={{
-                  color: '#E83535',
-                  textAlign: 'center',
-                  fontSize: 45,
-                  fontFamily: 'Cochin',
-                  paddingBottom: 15,
-                }}
-              >
-                Sliced
-              </Text>
-            </View>
-            <View>
-              <Image
-                source={pie}
-                style={styles.imagelogin}
-                resizeMode="cover"
-              />
-            </View>
-
-            <View style={{alignContent: 'center', flexDirection: 'column'}}>
-              <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                <Text style={styles.usernamelabel}>Email</Text>
-              </View>
-
-              <TextInput
-                style={styles.credentialinput}
-                onChangeText={(email) => this.setState({email})}
-              />
-
-              <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                <Text style={styles.usernamelabel}>Password</Text>
-              </View>
-              <TextInput
-                secureTextEntry={true}
-                style={styles.credentialinput}
-                onChangeText={(password) => this.setState({password})}
-              />
-            </View>
-
-            <TouchableOpacity
+  return (
+    <KeyboardAwareScrollView contentContainerStyle={{flex: 1}}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'white',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            height: '100%',
+          }}
+        >
+          <View>
+            <Text
               style={{
-                marginLeft: 40,
-                marginRight: 40,
-                marginTop: 20,
-                height: 48,
-                width: 300,
-                borderRadius: 15,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#E83535',
-                marginBottom: 20,
+                color: '#E83535',
+                textAlign: 'center',
+                fontSize: 45,
+                fontFamily: 'Cochin',
+                paddingBottom: 15,
               }}
-              onPress={this.handlePress}
             >
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 30,
-                  fontFamily: 'Cochin',
-                }}
-              >
-                Login
-              </Text>
-            </TouchableOpacity>
+              Sliced
+            </Text>
           </View>
-        </TouchableWithoutFeedback>
-      </KeyboardAwareScrollView>
-    )
-  }
+          <View>
+            <Image source={pie} style={styles.imagelogin} resizeMode="cover" />
+          </View>
+
+          {loginFailed ? (
+            <Text>Sorry, unable to login with those credentials</Text>
+          ) : null}
+
+          <View style={{alignContent: 'center', flexDirection: 'column'}}>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <Text style={styles.usernamelabel}>Email</Text>
+            </View>
+
+            <TextInput
+              style={styles.credentialinput}
+              onChangeText={(email) => setEmail(email)}
+              ref={(input) => {
+                emailInput = input
+              }}
+            />
+
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <Text style={styles.usernamelabel}>Password</Text>
+            </View>
+            <TextInput
+              secureTextEntry={true}
+              style={styles.credentialinput}
+              onChangeText={(password) => setPassword(password)}
+              ref={(input) => {
+                passwordInput = input
+              }}
+            />
+          </View>
+
+          <TouchableOpacity
+            style={{
+              marginLeft: 40,
+              marginRight: 40,
+              marginTop: 20,
+              height: 48,
+              width: 300,
+              borderRadius: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#E83535',
+              marginBottom: 20,
+            }}
+            onPress={handlePress}
+          >
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 30,
+                fontFamily: 'Cochin',
+              }}
+            >
+              Login
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAwareScrollView>
+  )
 }
 
 const mapState = (state) => {
